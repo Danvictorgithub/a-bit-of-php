180b82a6debedfbfcc90e6bcabbdf796f9b102b0
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-01-03 14:24:05 +0800

    learning loops

---
+++ loops.php
@@ -0,0 +1,27 @@
+<?php
+
+/* $x = 1; */
+/* while (true) { */
+/* 	$x++; */
+/* 	echo "{$x}\n"; */
+/* } */
+
+
+/* $x = 1; */
+/* while (true) { */
+/* 	$x++; */
+/* 	echo "{$x}\n"; */
+/* 	if ($x > 10) { */
+/* 		break; */
+/* 	} */
+/* } */
+
+/* $colors = array("red", "green", "blue", "yellow"); */
+/**/
+/* foreach ($colors as $value) { */
+/* 	echo "$value\n"; */
+/* } */
+
+$colors = array("red", "green", "blue", "yellow");
+
+echo var_dump($colors);
