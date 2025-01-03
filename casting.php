1a924da57da51817d41b8b5e8cee09f654402de6
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-01-03 13:47:19 +0800

    learning casting

---
+++ casting.php
@@ -0,0 +1,53 @@
+<?php
+$a = 5; // Integer
+$b = 5.34; // Float
+$c = "hello"; // String
+$d = true; // Boolean
+$e = NULL; // NULL
+$f = true;    // Boolean
+$g = NULL;    // NULL
+$h = true;    // Boolean
+$i = NULL;    // NULL
+
+/* $a = (string) $a; */
+/* $b = (string) $b; */
+/* $c = (string) $c; */
+/* $d = (string) $d; */
+/* $e = (string) $e; */
+
+/* $a = (int) $a; */
+/* $b = (int) $b; */
+/* $c = (int) $c; */
+/* $d = (int) $d; */
+/* $e = (int) $e; */
+/* $f = (int) $f; */
+/* $g = (int) $g; */
+
+/* $a = (float) $a; */
+/* $b = (float) $b; */
+/* $c = (float) $c; */
+/* $d = (float) $d; */
+/* $e = (float) $e; */
+/* $f = (float) $f; */
+/* $g = (float) $g; */
+
+$a = (bool) $a;
+$b = (bool) $b;
+$c = (bool) $c;
+$d = (bool) $d;
+$e = (bool) $e;
+$f = (bool) $f;
+$g = (bool) $g;
+$h = (bool) $h;
+$i = (bool) $i;
+
+//To verify the type of any object in PHP, use the var_dump() function:
+var_dump($a);
+var_dump($b);
+var_dump($c);
+var_dump($d);
+var_dump($e);
+var_dump($f);
+var_dump($g);
+var_dump($h);
+var_dump($i);
---
+++ numbers.php
@@ -0,0 +1,7 @@
+<?php
+
+$x = 69420.69;
+$y = "420";
+
+/* echo (int)$x; */
+echo (float)$y;
