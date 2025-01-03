425a022e0e4c1614737c0b85e92cc619e0cc8d07
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-01-03 15:45:37 +0800

    learning arrays and wtf php

---
+++ arrays.php
@@ -0,0 +1,39 @@
+<?php
+
+/* $cars = array("Volvo", "BMW", "Toyota"); */
+/**/
+/* $cars_info = array("Volvo" => "Sweden", "BMW" => "Germany", "Toyota" => "Japan"); */
+/**/
+/* echo $cars_info["Volvo"]; */
+/**/
+/**/
+/* $cars_array = ["Volvo", "BMW", "Toyota"]; */
+/**/
+/* print_r($cars_array); */
+
+
+/* $cars_ass = ["memes" => "shit"]; */
+/**/
+/* echo $cars_ass["memes"]; */
+
+
+/* $cars = array("Volvo", "BMW", "Toyota"); */
+/* foreach ($cars as &$x) { */
+/* 	$x = "Ford"; */
+/* } */
+/* unset($x); // What the fuck is this */
+/**/
+/* print($x); */
+
+
+/* $fruits = ["Apple", "Banana", "Cherry"]; */
+/* array_push($fruits, "Orange", "Bayabas"); */
+/**/
+/* print_r($fruits); */
+
+
+/* $cars = array("brand" => "Ford", "model" => "Mustang"); */
+/* $cars += ["memes" => "I hate php"]; */
+/**/
+/* array_splice($cars, -1, 1); */
+/* print_r($cars); */
---
+++ testing.js
@@ -0,0 +1,8 @@
+cars = ["Volvo", "BMW", "Ford"];
+
+for (let car in cars) {
+  car = "Ford";
+  console.log(car);
+}
+// ok what the hell php
+console.log(car);
