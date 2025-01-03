ebb0bfdcac7ac88aef7b9d1f0577d703e79237d8
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-01-03 13:59:58 +0800

    learning math php

---
+++ math.php
@@ -0,0 +1,17 @@
+<?php
+
+/* echo (pi()); */
+
+/* echo (min("1", 0, [-1])); */
+
+/* echo (abs(-6.9)); */
+
+
+/* echo (sqrt(64)); */
+
+
+/* echo (round(0.60)); */
+/* echo (round(0.50)); */
+
+
+echo (rand(10, 100));
